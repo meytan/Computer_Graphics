4da9897f4896b9233f25daef1b381b96be6ccd60
meytn <szifugupek4>
2019-04-25 18:01:20 +0200

    Added Lab6

---
+++ CG_Lab6/CG_Lab6/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace CG_Lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(33, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(767, 527);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(845, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(845, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 30);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(845, 143);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 30);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 566);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ CG_Lab6/CG_Lab6/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CG_Lab6
+{
+    public partial class Form1 : Form
+    {
+        private System.Drawing.Graphics g;
+        private System.Drawing.Pen pen1 = new System.Drawing.Pen(Color.Blue, 2);
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            PointF[] points =
+            {
+                new PointF(50,500),
+                new PointF(100,50),
+                new PointF(400,20),
+                new PointF(500,480)
+            };
+            
+            for(float t = 0; t<1; t += 0.0001f)
+            {
+                float y = (1 - t) * (1 - t) * (1 - t) * points[0].Y + (1 - t) * (1 - t) * 3 * t * points[1].Y + 3 * t * t * (1 - t) * points[2].Y + t * t * t * points[3].Y;
+                float x = (1 - t) * (1 - t) * (1 - t) * points[0].X + (1 - t) * (1 - t) * 3 * t * points[1].X + 3 * t * t * (1 - t) * points[2].X + t * t * t * points[3].X;
+
+                g.FillRectangle((Brush)Brushes.Blue, x, y, 1, 1);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            PointF P1 = new PointF(10, 100);
+            PointF P2 = new PointF(100,100);
+            
+
+
+            for (float t = 0.001f; t < 1; t += 0.001f)
+            {
+                
+                float x = (2 * t * t * t - 3 * t * t + 1) * P1.X + (-2 * t * t * t + 3 * t * t) * P2.X + (t * t * t - 2 * t * t + t) * 10 + (t * t * t - t * t) * 10;
+                float y = (2 * t * t * t - 3 * t * t + 1) * P1.Y + (-2 * t * t * t + 3 * t * t) * P2.Y + (t * t * t - 2 * t * t + t) * 1000 + (t * t * t - t * t) * 1000;
+                g.FillRectangle((Brush)Brushes.Blue, x, y, 1, 1);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            PointF P1 = new PointF(10, 10);
+            PointF P2 = new PointF(30, 250);
+            PointF P3 = new PointF(320, 300);
+            PointF P4 = new PointF(400, 450);
+            PointF P5 = new PointF(430, 30);
+            PointF P6 = new PointF(390, 100);
+            PointF P7 = new PointF(90, 100);
+
+            /*
+            for (float t = 0.001f; t < 1; t += 0.001f)
+            {
+
+                float x =  ((((1 - t) * (1 - t) * (1 - t)) * P1.X) / 6) + (((3 * t * t * t - 6 * t * t + 4) * P2.X) / 6) +((-3 * t * t * t + 3 * t * t + 3 * t + 1) * P3.X) / 6 + (t * t * t * P4.X) / 6;
+                float y =  ((((1 - t) * (1 - t) * (1 - t)) * P1.Y) / 6) + (((3 * t * t * t - 6 * t * t + 4) * P2.Y) / 6) +((-3 * t * t * t + 3 * t * t + 3 * t + 1) * P3.Y) / 6 + (t * t * t * P4.Y) / 6;
+                
+                g.FillRectangle((Brush)Brushes.Blue, x, y, 1, 1);
+            }
+
+            for (float t = 0.001f; t < 1; t += 0.001f)
+            {
+
+                float x = ((((1 - t) * (1 - t) * (1 - t)) * P3.X) / 6) + (((3 * t * t * t - 6 * t * t + 4) * P4.X) / 6) + ((-3 * t * t * t + 3 * t * t + 3 * t + 1) * P5.X) / 6 + (t * t * t * P6.X) / 6;
+                float y = ((((1 - t) * (1 - t) * (1 - t)) * P3.Y) / 6) + (((3 * t * t * t - 6 * t * t + 4) * P4.Y) / 6) + ((-3 * t * t * t + 3 * t * t + 3 * t + 1) * P5.Y) / 6 + (t * t * t * P6.Y) / 6;
+
+                g.FillRectangle((Brush)Brushes.Blue, x, y, 1, 1);
+            }*/
+
+            for (float t = 0; t < 1; t += 0.0001f)
+            {
+                float y = (1 - t) * (1 - t) * (1 - t) * P1.Y + (1 - t) * (1 - t) * 3 * t * P2.Y + 3 * t * t * (1 - t) * P3.Y + t * t * t * P6.Y;
+                float x = (1 - t) * (1 - t) * (1 - t) * P1.X + (1 - t) * (1 - t) * 3 * t * P2.X + 3 * t * t * (1 - t) * P3.X + t * t * t * P6.X;
+
+                g.FillRectangle((Brush)Brushes.Blue, x, y, 1, 1);
+
+               
+            }for (float t = 0; t < 1; t += 0.0001f)
+            {
+                float y = (1 - t) * (1 - t) * (1 - t) * P6.Y + (1 - t) * (1 - t) * 3 * t * P5.Y + 3 * t * t * (1 - t) * P3.Y + t * t * t * P4.Y;
+                float x = (1 - t) * (1 - t) * (1 - t) * P6.X + (1 - t) * (1 - t) * 3 * t * P5.X + 3 * t * t * (1 - t) * P3.X + t * t * t * P4.X;
+
+                g.FillRectangle((Brush)Brushes.Blue, x, y, 1, 1);
+            }
+
+            for (float t = 0; t < 1; t += 0.0001f)
+            {
+                float y = (1 - t) * (1 - t) * (1 - t) * P4.Y + (1 - t) * (1 - t) * 3 * t * P5.Y + 3 * t * t * (1 - t) * P3.Y + t * t * t * P7.Y;
+                float x = (1 - t) * (1 - t) * (1 - t) * P4.X + (1 - t) * (1 - t) * 3 * t * P5.X + 3 * t * t * (1 - t) * P3.X + t * t * t * P7.X;
+
+                g.FillRectangle((Brush)Brushes.Blue, x, y, 1, 1);
+            }
+        }
+    }
+}
